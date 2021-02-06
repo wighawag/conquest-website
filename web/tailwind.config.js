09f626c9ddd73f52f109a9249ace77cd8daea74f
Wighawag <wighawag@gmail.com>
2021-02-06 09:59:53 +0000

    fresh from jolly-roger

---
+++ web/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  purge: ['./src/**/*.html', './src/**/*.svelte'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
